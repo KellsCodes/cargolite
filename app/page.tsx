f8ad760a476f1a6a6b11a63c62892c966e6a4279
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2025-12-30 11:24:42 +0100

    feat: add slanted background layout and smooth arrow hover animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+"use client"
 import Image from "next/image";
 import Layout from "./components/Layout";
 import { HeroSection } from "./components/HeroSection";
 import { OFFERS } from "@/lib/offer";
-import { Check } from "lucide-react";
+import { ChartColumn, Check, MoveRight, MoveUpRight } from "lucide-react";
+import Link from "next/link";
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
@@ -65,6 +68,68 @@ export default function Home() {
                 <li className="flex items-center gap-x-2"><Check className="w-5 text-chart-2 stroke-[3px]" /><span className="opacity-50">Share files, discuss</span></li>
                 <li className="flex items-center gap-x-2"><Check className="w-5 text-chart-2 stroke-[3px]" /><span className="opacity-50">Track time spent on each project</span></li>
               </ul>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="relative h-[600px] w-full my-12">
+          <div
+            className="absolute left-0 top-0 bottom-0 w-[64%] bg-[#FFE799]"
+            style={{ clipPath: 'polygon(0 0, calc(100% - 250px) 0, 100% 100%, 0 100%)' }}
+          />
+          <div
+            className="absolute right-0 top-0 bottom-0 w-[calc(36%+250px)]"
+            style={{
+              clipPath: 'polygon(0 0, 100% 0, 100% 100%, 250px 100%)',
+              backgroundImage: 'url("/port.png")',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center'
+            }}
+          />
+          <div className="relative z-10 w-[72vw] mx-auto space-y-6 text-header-top flex flex-col h-full justify-center">
+            <button className="text-xs p-3 bg-chart-5 w-[100px] no-pointer-cursor">Get in touch</button>
+            <h3 className="text-3xl font-medium w-xs">Proud to Deliver Excellence Every Time</h3>
+            <p className="w-3xl text-md">We are committed to maintaining the highest standards of professional service for every client. Our dedicated team works tirelessly to ensure that every project reflects our core values of integrity, precision, and superior quality.</p>
+            <div className="w-[50%] grid grid-cols-2 gap-x-8 mt-6">
+              <div className="space-y-2">
+                <p className="flex items-center gap-x-1 font-medium"><ChartColumn className="w-4" /><span>Strategic Growth</span></p>
+                <p className="text-xs">Leverage data-driven insights to scale your operations with precision and long-term stability.</p>
+              </div>
+              <div className="space-y-2">
+                <p className="flex items-center gap-x-1 font-medium"><ChartColumn className="w-4" /><span>Optimized Performance</span></p>
+                <p className="text-xs">Our workflows are refined to deliver maximum efficiency without compromising our signature quality.</p>
+              </div>
+            </div>
+
+            <div className="mt-6 flex items-center gap-x-10">
+              <Link href="/contact-us" className="bg-[#034460] hover:bg-[#FEC201] transition-all duration-500 ease-in-out text-white text-sm p-4 w-[100px]">Contact us</Link>
+              <motion.button
+                initial="initial"
+                whileHover="hover"
+                className="flex items-center gap-x-2 outline-none group"
+              >
+                <Link href="/about" className="text-sm font-medium flex items-center gap-x-1">
+                  <button>
+                    <span>Learn more</span>
+                    <motion.span
+                      style={{ originX: 0, originY: 1 }}
+                      variants={{
+                        initial: { rotate: 0 },
+                        hover: { rotate: -30 }
+                      }}
+                      transition={{
+                        type: "spring",
+                        stiffness: 100,
+                        damping: 10
+                      }}
+                      className="w-full h-full flex items-center justify-center"
+                    >
+                      <MoveRight className="w-4 h-4" />
+                    </motion.span>
+                  </button>
+                </Link>
+              </motion.button>
             </div>
           </div>
         </div>
