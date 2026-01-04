743e11e6297162447d973d99aeb44ce646d6cc6c
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-01-04 03:06:37 +0100

    feat(ui): add footer to the layout

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Facebook, Instagram, Mail, Phone, Twitter, Youtube } from "lucide-react";
+import { Clock, Facebook, Instagram, Mail, MapPin, Phone, Twitter, Youtube } from "lucide-react";
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence, useScroll, useMotionValueEvent } from "motion/react"
 import Link from "next/link";
@@ -106,17 +106,76 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
             <main className="w-full flex-1 mx-auto">{children}</main>
 
-            <footer className="bg-header-top text-center h-20">
-                <div className="w-[72vw] h-full mx-auto text-white/40 flex items-center justify-between">
-                    <p>
-                        {new Date().getFullYear()} © Cargolite. All rights reserved.
-                    </p>
+            <footer className="bg-[#034460]">
+                <div className="w-[72vw] mx-auto grid grid-cols-3 gap-x-5 py-24 text-white">
+                    <div className="col-span-1 space-y-4">
+                        <h3 className="font-medium text-chart-5 text-xl">CARGOLITE</h3>
+                        <p className="leading-7">We fuse our global network with our depth of expertise in air freight, ocean freight, railway transportation, trucking, and multimode transportation, also we are providing sourcing, warehousing, E-commercial fulfillment, and value-added service to our customers including kitting, assembly, customized package and business inserts, etc</p>
+                        <div className="flex items-center gap-x-6">
+                            <a href="https://x.com" target="_blank">
+                                <Twitter className="fill-white hover:fill-chart-5 w-5 hover:text-chart-5 transition-all ease-in-out duration-300" />
+                            </a>
 
-                    <ul className="flex items-center gap-x-6 text-sm">
-                        <li><Link href={"#"}>Privacy Policy</Link></li>
-                        <li><Link href={"#"}>Cookies</Link></li>
-                        <li><Link href={"#"}>Refund Policy</Link></li>
-                    </ul>
+                            <a href="https://facebook.com" target="_blank">
+                                <Facebook className="fill-white hover:fill-chart-5 w-5 hover:text-chart-5 transition-all ease-in-out duration-300" />
+                            </a>
+
+                            <a href="https://instagram.com" target="_blank">
+                                <Instagram className="w-5 hover:text-chart-5 transition-all ease-in-out duration-300" />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="col-span-1 pl-10 space-y-4">
+                        <h3 className="font-medium text-chart-5 text-xl">Useful Links</h3>
+                        <ul className="list-none flex flex-col gap-y-6">
+                            <li>
+                                <Link href={"/"} className="hover:text-chart-5" >Home</Link>
+                            </li>
+                            <li>
+                                <Link href={"/about"} className="hover:text-chart-5" >About</Link>
+                            </li>
+                            <li>
+                                <Link href={"/track-parcel"} className="hover:text-chart-5" >Track Parcel</Link>
+                            </li>
+                            <li>
+                                <Link href={"/contact-us"} className="hover:text-chart-5" >Contact Us</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-span-1 space-y-4">
+                        <h3 className="font-medium text-chart-5 text-xl">Contact Info</h3>
+                        <ul className="list-none flex flex-col gap-y-5 ">
+                            <li className="flex items-center gap-x-2 hover:text-chart-5">
+                                <Mail className="w-5" />
+                                <a href="mailto:contact@cargolite.com">contact@cargolite.com</a>
+                            </li>
+                            <li className="flex items-center gap-x-2 hover:text-chart-5">
+                                <Phone className="w-5" />
+                                <a href="tel:+44 20 7123 4567">+44 20 7123 4567</a>
+                            </li>
+                            <li className="flex items-center gap-x-2 hover:text-chart-5">
+                                <MapPin className="w-5" />
+                                <span>24 Holborn Viaduct, London EC1A 2BN, United Kingdom</span>
+                            </li>
+                            <li className="flex items-center gap-x-2 hover:text-chart-5">
+                                <Clock className="w-5" />
+                                <span>8:00 - 17:00, Mon - Sat</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="bg-header-top text-center h-20">
+                    <div className="w-[72vw] h-full mx-auto text-white/40 flex items-center justify-between">
+                        <p>
+                            {new Date().getFullYear()} © Cargolite. All rights reserved.
+                        </p>
+
+                        <ul className="flex items-center gap-x-6 text-sm">
+                            <li><Link href={"#"}>Privacy Policy</Link></li>
+                            <li><Link href={"#"}>Cookies</Link></li>
+                            <li><Link href={"#"}>Refund Policy</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </footer>
         </div>
