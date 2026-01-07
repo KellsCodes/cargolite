8a1055faa2195e1f950496e9ea64f5a5cc14e54d
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-01-07 15:33:28 +0100

    feat(ui): add faq on track-parcel page

--- app/track-parcel/page.tsx
+++ app/track-parcel/page.tsx
@@ -3,6 +3,12 @@ import { useState } from "react";
 import Layout from "../components/Layout";
 import Link from "next/link";
 import { ArrowRight, ChartArea, ChartLine, CircleStop, Rocket } from "lucide-react";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function TrackParcel() {
     const [trackError, setTrackError] = useState(false)
@@ -71,6 +77,73 @@ export default function TrackParcel() {
                 </div>
                 <div className="">
                     <img src="/img-info.png" alt="." loading="lazy" className="" />
+                </div>
+            </div>
+
+            <div className="w-[72vw] mx-auto grid grid-cols-2 gap-x-8 mb-[1px]">
+                <div className="space-y-6">
+                    <h2 className="Font-bold text-4xl">FAQs</h2>
+                    <p className="text-black/50">Feeling inquisitive? Have a read through some of our FAQs or contact our support for help</p>
+                    <div className="grid grid-cols-2 gap-x-5">
+                        <div className="col-span-2" >
+                            <img src="/faq1.png" alt="faq1" loading="lazy" />
+                        </div>
+                    </div>
+                    <div className="col-span-2 grid grid-cols-2 gap-x-5">
+                        <img src="/faq2.png" alt="faq2" loading="lazy" className="col-span-1" />
+                        <img src="/faq3.png" alt="faq2" loading="lazy" className="col-span-1 h-full cover" />
+                    </div>
+                </div>
+
+                <div className="col-span-1">
+                    <Accordion
+                        type="single"
+                        collapsible
+                        className="w-full space-y-2"
+                        defaultValue="item-1"
+                    >
+                        <AccordionItem value="item-1" className="bg-[#034460] text-white px-4">
+                            <AccordionTrigger className="[&>svg]:text-white">How do I track my package</AccordionTrigger>
+                            <AccordionContent className="flex flex-col gap-4 text-balance">
+                                <p>
+                                    You can track your package by entering your tracking number in the search field above. Your tracking number is provided in your shipping confirmation email or receipt.
+                                </p>
+                            </AccordionContent>
+                        </AccordionItem>
+
+                        <AccordionItem value="item-2" className="bg-[#034460] text-white px-4">
+                            <AccordionTrigger className="[&>svg]:text-white">What if my package is delayed?</AccordionTrigger>
+                            <AccordionContent className="flex flex-col gap-4 text-balance">
+                                <p>
+                                    If your package is experiencing delays, you'll see updated delivery estimates in your tracking information. For additional assistance, please contact our customer support team.
+                                </p>
+                            </AccordionContent>
+                        </AccordionItem>
+
+                        <AccordionItem value="item-3" className="bg-[#034460] text-white px-4">
+                            <AccordionTrigger className="[&>svg]:text-white">Can I change my delivery address?</AccordionTrigger>
+                            <AccordionContent className="flex flex-col gap-4 text-balance">
+                                <p>
+                                    Yes, in most cases you can change your delivery address by contacting our customer support team with your tracking number and the new delivery details.
+                                </p>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <div className="h-[1px] bg-black/10 my-10" />
+                    <div className="space-y-10">
+                        <h2 className="text-4xl font-medium text-[#034460]">Need more help?</h2>
+                        <div className="flex items-center gap-x-6">
+                            <Link href={"/contact-us"}>
+                                <button className="bg-chart-5 hover:bg-[#034460] text-sm text-white h-12 w-28 transition-all duration-500 ease-in-out cursor-pointer">Contact us</button>
+                            </Link>
+                            <Link href={"/about"}>
+                                <button className="h-12 w-28 cursor-pointer flex items-center gap-x-1 group">
+                                    Learn more <ArrowRight className="w-5 group-hover:rotate-[-30deg] transition-all duration-300 ease-in-out" />
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout>
