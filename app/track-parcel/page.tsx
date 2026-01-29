409ef280b5e7138a42fe85abb069bb6f2167aa84
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-01-29 16:58:13 +0100

    feat(ui): add admin parcel tracking - Made the tracking parcel component modular in one component - Made the Parcel details component modular to be single component

--- app/track-parcel/page.tsx
+++ app/track-parcel/page.tsx
@@ -10,31 +10,15 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 import TrackData from "./TrackDetails";
+import TrackingParcel from "../components/TrackParcel";
 
 export default function TrackParcel() {
     const [trackError, setTrackError] = useState(false)
     return (
         <Layout>
-            <div className="w-[72vw] mx-auto h-auto bg-[#034460] p-14 mb-10 space-y-6 flex flex-col items-center relative"
-                style={{
-                    backgroundImage: "url(/banner.png)",
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            >
-                <h1 className="font-bold text-4xl text-white">Track Your Shipment</h1>
-                <p className="w-[40%] text-center text-white text-lg">Get real-time updates on your package location, estimated delivery date and current status</p>
-                <div className="flex flex-col items-center space-y-3">
-                    <div className="flex items-center justify-center gap-x-4 w-full">
-                        <input type="text" className="h-14 w-[450px] bg-white py-4 px-5 rounded-md" placeholder="Enter tracking number (e.g., EDF123456789)" />
-                        <button className="h-14 text-md bg-white w-34 rounded-md font-semibold text-[#034460] cursor-pointer">Track Parcel</button>
-                    </div>
-                    {!trackError ?
-                        <p className="font-bold text-white/70 text-sm">Ready to track? Enter your ID above. Having trouble? <Link href={"/contact-us"} className="text-chart-5/90">Contact Support.</Link></p>
-                        :
-                        <p className="text-red-300 font-bold text-sm">Tracking ID not recognized. If you just shipped your item, please allow up to 24 hours for the status to update.</p>
-                    }
-                </div>
+            <div className="w-[72vw] mx-auto">
+                <TrackingParcel trackError={trackError} />
+
             </div>
 
             <div className="w-[72vw] mx-auto mt-20">
---
+++ app/tracking/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useState } from "react";
+import AuthLayout from "../components/AuthLayout";
+import TrackData from "../track-parcel/TrackDetails";
+import TrackingParcel from "../components/TrackParcel";
+
+export default function TrackingPage() {
+    const [trackError, setTrackError] = useState(false)
+    return (
+        <AuthLayout>
+            <div className="space-y-6">
+                <div>
+                    <h1 className="font-medium">Tracking Shipments</h1>
+
+                </div>
+
+                <div className="bg-white rounded-lg h-[74vh] overflow-hidden py-6">
+                    <div className="flex flex-col h-full">
+
+                        {/* The scrollable area */}
+                        <div className="flex-1 overflow-y-auto custom-scroll px-6">
+                            <TrackingParcel trackError={trackError} />
+                            <TrackData />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </AuthLayout>
+    )
+}
