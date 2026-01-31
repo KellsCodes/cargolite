b39dfaede979567b99393a322e9121af2a657152
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-01-31 11:45:00 +0100

    feat(ui): add mobile screen layout

--- app/components/AuthLayout.tsx
+++ app/components/AuthLayout.tsx
@@ -2,7 +2,7 @@
 import React, { ReactNode } from "react";
 import LogoText from "./LogoText";
 import Link from "next/link";
-import { BellIcon, Icon, LogOut, Search } from "lucide-react";
+import { BellIcon, Icon, LogOut, Search, TextAlignStart } from "lucide-react";
 import { authMenu } from "@/lib/authMenu";
 import { usePathname } from "next/navigation";
 
@@ -14,7 +14,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
     const pathname = usePathname()
     return (
         <div className="h-screen flex flex-col md:flex-row bg-white relative overflow-x-hidden">
-            <aside className="w-full md:w-64 shrink-0 border-r-3 border-r-black/3">
+            <aside className="hidden md:block md:w-64 shrink-0 border-r-3 border-r-black/3">
                 <div className="h-16 border-b-3 border-b-black/3 flex items-center pl-7">
                     <Link href="/dashboard">
                         <LogoText />
@@ -25,7 +25,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                         {
                             authMenu.map((item, i) => (
                                 <button key={i} className={`border-l-3 ${!pathname.startsWith(item.link) ? "border-l-white text-black/70" : "border-l-main-primary/70 text-main-primary"}`}>
-                                    <Link href={item.link} className={`flex items-center gap-x-2 h-13 pl-6 text-sm hover:text-chart-5/90 ${pathname === item.link && "bg-main-primary/2"} transition-all duration-300 ease-in-out`}>
+                                    <Link href={item.link} className={`flex items-center gap-x-2 h-13 pl-6 text-sm hover:text-chart-5/90 ${pathname.startsWith(item.link) && "bg-main-primary/2"} transition-all duration-300 ease-in-out`}>
                                         {item.icon}
                                         {item.name}
                                     </Link>
@@ -44,9 +44,9 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                 </div>
             </aside>
             <div className="flex-1 flex flex-col min-w-0">
-                <nav className="h-16 border-b-3 border-b-black/3 flex items-center justify-between px-6 bg-white">
+                <nav className="h-16 border-b-3 border-b-black/3 flex items-center justify-between px-4 md:px-6 bg-white">
                     {/* Search Area */}
-                    <div className="flex items-center gap-x-0 border border-black/10 rounded-lg px-3 shadow-sm bg-transparent">
+                    <div className="hidden md:flex items-center gap-x-0 border border-black/10 rounded-lg px-3 shadow-sm bg-transparent">
                         <Search className="w-4 text-black/50" />
                         <input
                             type="search"
@@ -54,6 +54,10 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                             className="p-2 outline-0 text-black/60 text-sm w-64 focus:w-80 transition-all duration-300"
                         />
                     </div>
+
+                    <button className="md:hidden">
+                        <TextAlignStart className="text-black/70" />
+                    </button>
 
                     {/* Notification */}
                     <div className="flex items-center gap-x-6">
@@ -63,7 +67,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                         </div>
 
                         {/* Welcome Message */}
-                        <div className="pl-6 border-l-2 border-black/5 flex flex-col justify-center h-10">
+                        <div className="pl-6 border-l-2 border-black/5 hidden md:flex flex-col justify-center h-10">
                             <p className="text-[10px] uppercase font-bold text-black/40 tracking-widest leading-none">Account</p>
                             <p className="text-sm font-semibold text-black/80">Hello, Joshua</p>
                         </div>
@@ -71,13 +75,27 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
                 </nav>
 
 
-                <main className="p-6 flex-1 min-w-0 bg-[#F4F6F8]/40">
+                <main className="hidden md:block p-6 flex-1 min-w-0 bg-[#F4F6F8]/40">
                     {children}
                 </main>
 
             </div>
 
-            <div className="absolute bottom-0 left-0 right-0 h-12 bg-white" />
+            <div className="hidden md:block absolute bottom-0 left-0 right-0 h-12 bg-white" />
+
+            {/* Mobile menu */}
+            <div className="md:hidden absolute bottom-0 left-0 right-0 py-2 bg-white border-t-2 border-t-black/3 grid grid-cols-4 gap-x-0 z-50">
+                {
+                    [...authMenu.slice(0, 3), authMenu[7]].map((item, i) => (
+                        <button key={i} className={`${!pathname.startsWith(item.link) ? "text-black/70" : "text-main-primary"}`}>
+                            <Link href={item.link} className={`flex flex-col gap-y-1 items-center text-xs hover:text-chart-5/90 transition-all duration-300 ease-in-out`}>
+                                {item.icon}
+                                {item.name}
+                            </Link>
+                        </button>
+                    ))
+                }
+            </div>
         </div>
     )
 }
