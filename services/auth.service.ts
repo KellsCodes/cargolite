03d94b30524630a20ec593cd0649e83b2e33587f
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-02-07 16:30:51 +0100

    feat(auth): use create token and email services on create user service

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,36 +1,56 @@
 import { hashPassword } from "@/lib/password";
 import prisma from "@/lib/prisma";
 import { RegisterSchema } from "@/schema/register.schema";
+import { createOtp } from "./token.service";
+import { OtpType } from "@/generated/prisma/enums";
+import { sendOtpEmail } from "@/lib/mail";
 
 export const registerUser = async (data: any) => {
   // Validate data
   const validatedData = RegisterSchema.parse(data);
-
   // Hash password
   const hashedPassword = await hashPassword(validatedData.password);
+
+  let registrationResult;
+
   try {
-    return await prisma.user.create({
-      data: {
-        email: validatedData.email,
-        password: hashedPassword,
-        role: validatedData.role,
-        profile: {
-          create: {
-            firstName: validatedData.firstName,
-            lastName: validatedData.lastName,
-            telephone: validatedData.phone,
-            region: validatedData.region,
+    registrationResult = await prisma.$transaction(async (tx) => {
+      const newUser = await tx.user.create({
+        data: {
+          email: validatedData.email.toLowerCase(),
+          password: hashedPassword,
+          role: validatedData.role,
+          profile: {
+            create: {
+              firstName: validatedData.firstName,
+              lastName: validatedData.lastName,
+              telephone: validatedData.phone,
+              region: validatedData.region,
+            },
           },
         },
-      },
-      select: {
-        id: true,
-        email: true,
-      },
+        select: {
+          id: true,
+          email: true,
+        },
+      });
+      const otp = await createOtp(newUser.id, OtpType.SIGNUP, tx);
+      return { newUser, otp };
     });
+    await sendOtpEmail(
+      registrationResult.newUser.email,
+      registrationResult.otp.code,
+      registrationResult.otp.type
+    );
+    return registrationResult.newUser;
   } catch (error: any) {
-    if (error.code === "P2002" && error.meta?.target?.includes("email")) {
-      throw new Error("USER_EXISTS");
+    // Check for Prisma's P2002 code
+    if (error.code === "P2002") {
+      // If the error involves the email field
+      const target = error.message;
+      if (target.includes("email")) {
+        throw new Error("USER_EXISTS");
+      }
     }
     throw new Error("DATABASE_ERROR");
   }
