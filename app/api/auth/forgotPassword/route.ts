efed9cc00e66504a903253d20230d4edb3276b6a
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-02-12 15:07:00 +0100

    refactor(password): rename forgotpassword to forgotPassword

---
+++ app/api/auth/forgotPassword/route.ts
@@ -0,0 +1,30 @@
+import { forgotPasswordRequest } from "@/services/auth.service";
+import { NextResponse } from "next/server";
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    await forgotPasswordRequest(body.email);
+    return NextResponse.json(
+      { success: true, message: "Password reset code sent to email." },
+      { status: 201 }
+    );
+  } catch (error: any) {
+    if (error.message === "INVALID_EMAIL") {
+      return NextResponse.json(
+        { error: "Invalid email." },
+        { status: 409 }
+      );
+    }
+    if (error instanceof SyntaxError) {
+      return NextResponse.json(
+        { error: "Invalid JSON format" },
+        { status: 400 }
+      );
+    }
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
