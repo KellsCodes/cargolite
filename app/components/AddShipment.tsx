b7a2cd277a2c309761dd42632f79c6242a3f1574
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-05-07 13:01:52 +0100

    fix(style): use only plus icon for add shipment on medium to small screen types

--- app/components/AddShipment.tsx
+++ app/components/AddShipment.tsx
@@ -4,12 +4,22 @@ import { Plus, Upload } from "lucide-react";
 export default function AddShipmentBtn() {
     return (
         <div className="flex items-center gap-x-5">
-            <button className="cursor-pointer flex items-center justify-center gap-x-1 w-[140px] hover:opacity-70 transition-all duration-300 ease-in-out h-12 bg-white text-sm p-5 rounded-sm">Export CSV <Upload className="w-4" /></button>
             <button
-                className="cursor-pointer flex items-center justify-center gap-x-1 w-[175px] hover:opacity-80 transition-all duration-300 ease-in-out h-12 bg-main-primary text-sm p-5 text-white rounded-sm"
+                className="cursor-pointer flex items-center justify-center gap-x-1 w-auto 2xl:w-[140px] hover:opacity-70 transition-all duration-300 ease-in-out h-12 bg-white text-sm p-5 rounded-sm"
+            >
+                <span className="hidden xl:inline-block">
+                    Export CSV
+                </span>
+                <Upload className="w-5 2xl:w-4" />
+            </button>
+            <button
+                className="cursor-pointer flex items-center justify-center gap-x-1 w-auto 2xl:w-[175px] hover:opacity-80 transition-all duration-300 ease-in-out h-12 bg-main-primary text-sm p-5 text-white rounded-sm"
                 onClick={() => { window.location.href = "/shipping/saveshipment" }}
             >
-                Add New Shipping <Plus className="w-4" />
+                <span className="hidden xl:inline-block">
+                    Add New Shipping
+                </span>
+                <Plus className="w-4" />
             </button>
         </div>
     )
