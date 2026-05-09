a9d34859d1567ca54e06d6c244c6424ad07537f6
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-05-09 15:39:28 +0100

    refactor: add type for shipment data

---
+++ app/shipping/types.ts
@@ -0,0 +1,43 @@
+import {
+  CourierType,
+  PackageType,
+  ShipmentStatus,
+} from "@/generated/prisma/enums";
+
+export interface TrackingHistory {
+  id: number;
+  status: ShipmentStatus;
+}
+
+export interface ShipmentInvoice {
+  amount: string;
+  payerRole: "SENDER" | "RECEIVER";
+}
+
+export interface ShipmentPerson {
+  name: string;
+}
+
+export interface Shipment {
+  id: number;
+  shipmentID: string;
+  weight: number;
+  amount: string;
+  arrival: string; // ISO Date String
+  clientName: string;
+  courierType: CourierType;
+  packageCount: number;
+  packageType: PackageType;
+  pickupLocation: string;
+  dropLocation: string;
+  packageImage: string; // Cloudinary URL
+  createdAt: string;
+  updatedAt: string;
+  sender: ShipmentPerson;
+  receiver: ShipmentPerson;
+  invoice: ShipmentInvoice;
+  trackingHistory: TrackingHistory[];
+  senderId: number;
+  receiverId: number;
+  invoiceId: number;
+}
