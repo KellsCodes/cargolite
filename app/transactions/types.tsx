b8f99a5fc70a4975555b81e5fa3bf34a96d91a9d
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-05-15 17:04:48 +0100

    chore: add invoice type for api response data structure

---
+++ app/transactions/types.tsx
@@ -0,0 +1,45 @@
+import { InvoiceStatus } from "@/generated/prisma/enums";
+
+export type PayerRole = "SENDER" | "RECEIVER"; // Expand as needed based on your DB schema
+export type PaymentMethod = "TRANSFER" | "CASH" | "CARD" | "POS";
+
+export interface InvoicePayload {
+    id: number;
+    transactionID: string;
+    amount: string | number;
+    invoiceStatus: InvoiceStatus;
+    payerRole: PayerRole;
+    paymentMethod: PaymentMethod;
+    createdAt: string;
+    updatedAt: string;
+    clientId: number;
+    client: ClientInfo;
+    shipment: ShipmentInfo
+}
+
+
+interface ClientInfo {
+    id: number;
+    name: string;
+    email: string
+}
+
+interface ShipmentInfo {
+    id: number;
+    shipmentID: string;
+    pickupLocation: string;
+    dropLocation: string;
+}
+
+
+export interface APIResponse {
+    data: InvoicePayload[];
+    meta: {
+        currentPage: number;
+        pageLength: number;
+        totalItems: number;
+        totalPages: number;
+        from: number;
+        to: number;
+    };
+}
