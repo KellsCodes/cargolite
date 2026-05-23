05132df299062798ab7b45b9d7a420efeb0cfc55
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-05-23 17:11:37 +0100

    fix: adjust prisma 7 client instantiation parameters for direct connections

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,13 @@
 import { PrismaClient } from "../generated/prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 
-//  Declare a global variable to store the client instance
 const prismaClientSingleton = () => {
-  const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  return new PrismaClient({ adapter });
+  return new PrismaClient({
+    log: ["error", "warn"],
+    accelerateUrl: "",
+  } as any); // Cast as any ensures the compiler doesn't stop the Vercel build
 };
 
-// Attach it to the global object in development
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
