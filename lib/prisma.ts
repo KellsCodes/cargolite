3b3bcd9decb93150a93176132cbef7484d4117c5
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-06-02 11:36:37 +0100

    fix: add ssl to connection string

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,28 +3,27 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import "dotenv/config";
 
 const prismaClientSingleton = () => {
-  // 1. Grab the active connection string (Vercel will use your Aiven URL)
   const connectionString = process.env.DATABASE_URL;
 
   if (!connectionString) {
     throw new Error("DATABASE_URL environment variable is missing.");
   }
 
-  // 2. Use the native URL parser to securely extract the cloud credentials
+  // Use the native URL parser to securely extract the cloud credentials
   const dbUrl = new URL(connectionString);
 
   const adapter = new PrismaMariaDb({
-    host: dbUrl.hostname, // Extracts: mysql-e9201fc-ifeanyinworji...
-    port: Number(dbUrl.port) || 3306, // Extracts: 20009
-    user: dbUrl.username, // Extracts: avnadmin
-    password: decodeURIComponent(dbUrl.password), // Extracts your secure password cleanly
-    database: dbUrl.pathname.replace("/", ""), // Extracts: defaultdb
+    host: dbUrl.hostname,
+    port: Number(dbUrl.port) || 3306,
+    user: dbUrl.username,
+    password: decodeURIComponent(dbUrl.password),
+    database: dbUrl.pathname.replace("/", ""),
+    ssl: true //dbUrl.searchParams.get("sslmode") === "require",
   });
 
   return new PrismaClient({ adapter });
 };
 
-// The rest of your exact global tracking file remains completely unchanged
 declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
