4139f0d7fdb80944b54c10d611c785c16c23f1db
Ifeanyi Nworji <nworjiifeanyi@gmail.com>
2026-06-02 21:56:30 +0100

    fix: use nodemailer for default mail transporter

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,28 +1,36 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
 import { OtpEmail } from "@/app/api/emails/OtpTemplate";
 import { render } from "@react-email/components";
 import { AdminMessageEmail } from "@/app/api/emails/AdminMessageTemplate";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 export const sendOtpEmail = async (to: string, otp: string, type: string) => {
   try {
     // Render React component to HTML string
     const emailHtml = await render(OtpEmail({ otp, type }));
 
-    const { data, error } = await resend.emails.send({
+    // const { data, error } = await resend.emails.send({
+    const { messageId } = await transporter.sendMail({
       from: "Cargolite <onboarding@resend.dev>", // Replace with your domain once verified
       to: [to],
       subject: "Verification Code - Cargolite",
       html: emailHtml,
     });
 
-    if (error) {
+    if (!messageId) {
       // console.error("Resend Error:", error);
-      return { success: false, error };
+      return { success: false, message: "Failed to send email" };
     }
 
-    return { success: true, data };
+    return { success: true, messageId };
   } catch (err) {
     // console.error("Mail Service Error:", err);
     return { success: false, error: err };
@@ -36,11 +44,17 @@ export const sendAdminMessageByEmail = async (
   senderName: string,
   attachmentUrls?: string[]
 ) => {
-  return await resend.emails.send({
+  // return await resend.emails.send({
+  return await transporter.sendMail({
     from: "Cargolite <onboarding@resend.dev>", // Replace with your domain once verified
     to: [to],
     subject,
-    react: AdminMessageEmail({ body, subject, senderName }),
-    attachments: attachmentUrls?.length ? attachmentUrls.map(url => ({ path: url, filename: url.split('/').pop() || 'attachment' })) : [],
+    html: await render(AdminMessageEmail({ body, subject, senderName })),
+    attachments: attachmentUrls?.length
+      ? attachmentUrls.map((url) => ({
+          path: url,
+          filename: url.split("/").pop() || "attachment",
+        }))
+      : [],
   });
 };
